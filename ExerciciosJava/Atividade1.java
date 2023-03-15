53c4ff960eab86b1875a2225c701f5e83e55dba1
Graziella Santos <trigiotti95@gmail.com>
2023-03-15 14:23:40 -0300

    ativdade java

---
+++ ExerciciosJava/Atividade1.java
@@ -0,0 +1,34 @@
+package Atividade;
+   import java.util.Scanner;
+   
+      public class Atividade1 {
+      public static void main(String[]args) {
+	//TODO Auto-generated method stub
+    	  
+    	  float salario, abono, novosalario;
+    	  
+    	  Scanner leia = new Scanner(System.in);
+    	  System.out.println("Coloque o salario:");
+    	  salario = (float)leia.nextDouble();
+    	  
+    	  System.out.println("Coloque o abono:");
+    	  abono = (float)leia.nextDouble();
+    	  
+    	  novosalario = salario + abono;
+    	  
+    	  System.out.printf("Novosalario:R$ %.2F",novosalario);
+    	  
+    	  
+    	  
+    	  
+
+    	  
+    	  
+    	  
+    		
+    			  
+	
+		
+}
+}
+
